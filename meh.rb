9fce91f47f7f0f5a619fde5d0db5035df3a2e2bc
David Wilkie <dwilkie@gmail.com>
2010-05-03 20:06:50 +0700

    added mailing using mail gem to capture the post request for auction ended

--- meh.rb
+++ meh.rb
@@ -1,6 +1,26 @@
 # meh.rb
 require 'sinatra'
 
-post '/orders/create' do
-  params.to_s
+require 'mail'
+options = { :address              => "smtp.gmail.com",
+            :port                 => 587,
+            :domain               => 'local.localhost',
+            :user_name            => ENV['GMAIL_USERNAME'],
+            :password             => ENV['GMAIL_PASSWORD'],
+            :authentication       => 'plain',
+            :enable_starttls_auto => true  }
+            
+Mail.defaults do
+  delivery_method :smtp, options
 end
+
+post '/orders/create' do
+  BODY = params.to_s
+  Mail.deliver do
+    to ENV['EMAIL_ADDRESS']
+    from 'dwilkie@gmail.com'
+    subject ENV['EMAIL_ADDRESS']
+    body BODY
+  end
+end
+
